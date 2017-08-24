93937ec3e22aea59c9b07e348785d039e1818cb5
Bonuspunkt <bonuspunkt+github@gmail.com>
2017-08-24 23:33:47 +0200

    step

--- bin/l4n-provider.js
+++ bin/l4n-provider.js
@@ -4,16 +4,26 @@ const l4nProvider = require('../');
 const fs = require('fs');
 const path = require('path');
 
-const settings = process.argv[1];
+const settingsFile = process.argv[2];
 
-if (!settings) {
+if (!settingsFile) {
     return console.log('Usage: l4n-provider <settings.js>');
 }
-const settingPath = path.resolve(settings);
-debug(`settings.js resolved to '${ settingPath }'`);
+const settingsPath = path.resolve(settingsFile);
+debug(`settings.js resolved to '${ settingsPath }'`);
 
-if (!fs.existsSync(settingPath)) {
-    return console.log(`settings.js was not found at ${ settingPath }`);
+if (!fs.existsSync(settingsPath)) {
+    return console.log(`settings.js was not found at ${ settingsPath }`);
 }
 
-l4nProvider(require(settingPath));
+const baseSettings = require(settingsPath);
+const settings = Object.assign({},
+    baseSettings,
+    {
+        gameProviders: baseSettings.games.map(game => {
+            const provider = require(`l4n-provider-${ game.name }`)
+            return provider(game);
+        })
+    });
+
+l4nProvider(settings);
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const UdpResponder = require('./lib/udpResponder');
+
 module.exports = function(settings) {
+    const { https, gameProviders } = settings;
 
-    require('./lib/udpResponder');
-    require('./lib/httpsServer')(settings.https);
+    const udpResponder = new UdpResponder();
+    udpResponder.listen();
 
-}
+    require('./lib/httpsServer')(https, gameProviders);
+
+};
