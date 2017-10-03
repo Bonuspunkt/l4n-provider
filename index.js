806c4c72a1b71c7bb907527df3556e2658795383
Bonuspunkt <bonuspunkt+github@gmail.com>
2017-10-03 21:34:14 +0200

    servers are spawnable

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const TlsServer = require('./lib/tlsServer');
 const UdpResponder = require('./lib/udpResponder');
-const Watchdog = require('./lib/watchdog');
 
 module.exports = ({ register, resolve }) => {
     const udpResponder = new UdpResponder(resolve);
@@ -8,9 +7,6 @@ module.exports = ({ register, resolve }) => {
 
     const tlsServer = new TlsServer(resolve);
     register('tlsServer', () => tlsServer);
-
-    const watchdog = new Watchdog(resolve);
-    register('watchdog', () => watchdog);
 
     udpResponder.listen();
     tlsServer.listen();
